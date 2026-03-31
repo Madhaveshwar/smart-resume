ff1c4599b0ad80b5d19835021f21295504a691de
Madhaveshwar <madhavnimmagadda@gmail.com>
2026-03-31 23:49:22 +0530

    remove target folder

--- backend/src/main/java/com/smartresume/SmartResumeApplication.java
+++ backend/src/main/java/com/smartresume/SmartResumeApplication.java
@@ -2,8 +2,10 @@ package com.smartresume;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages = "com.smartresume")
+@SpringBootApplication
+@ComponentScan(basePackages = "com.smartresume")
 public class SmartResumeApplication {
     public static void main(String[] args) {
         SpringApplication.run(SmartResumeApplication.class, args);

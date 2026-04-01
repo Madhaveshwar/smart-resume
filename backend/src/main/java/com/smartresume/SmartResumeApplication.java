13cf120a79fc733ba959bb3de10527d071afc180
Madhaveshwar <madhavnimmagadda@gmail.com>
2026-04-01 15:28:02 +0530

    remove component scan fix

--- backend/src/main/java/com/smartresume/SmartResumeApplication.java
+++ backend/src/main/java/com/smartresume/SmartResumeApplication.java
@@ -2,10 +2,8 @@ package com.smartresume;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.smartresume")
 public class SmartResumeApplication {
     public static void main(String[] args) {
         SpringApplication.run(SmartResumeApplication.class, args);
